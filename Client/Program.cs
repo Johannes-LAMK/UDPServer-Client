7709b4fdc7ad24693f10390f9ea562710139b2fe
jsyv <johannes.syvameri@student.lamk.fi>
2020-01-22 10:44:56 +0200

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint remoteEndPoint = new IPEndPoint(Dns.GetHostAddresses("127.0.0.1")[0], 12345);
+            EndPoint endPoint = (EndPoint)new IPEndPoint(IPAddress.Any, 12345);
+            byte[] bytesToSend;
+            byte[] buffer = new byte[1000];
+            string message;
+            Socket socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+            Console.WriteLine("Client started!");
+            bool run = true;
+            while (run)
+            {
+                Console.WriteLine("Give a message: ");
+                message = Console.ReadLine();
+                if (message == "")
+                {
+                    run = false;
+                    socket.SendTo(System.Text.Encoding.UTF8.GetBytes(""), remoteEndPoint);
+                    break;
+                }
+                bytesToSend = System.Text.Encoding.UTF8.GetBytes(message);
+                socket.SendTo(bytesToSend, remoteEndPoint);
+                socket.ReceiveFrom(buffer, ref endPoint);
+                Console.WriteLine("Got: " + System.Text.Encoding.UTF8.GetString(buffer, 0, message.Length));
+            }
+            socket.Close();
+            Console.WriteLine("Client stoppped!");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] buffer = new byte[1000];
+            int lengthOfReceivedMessage;
+            EndPoint remoteEndPoint = (EndPoint)new IPEndPoint(IPAddress.Any, 12345);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 12345);
+            Socket socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(localEndPoint);
+            Console.WriteLine("Server started!");
+            bool run = true;
+            while (run)
+            {
+                lengthOfReceivedMessage = socket.ReceiveFrom(buffer, ref remoteEndPoint);
+                if (lengthOfReceivedMessage == 0)
+                {
+                    run = false;
+                    break;
+                }
+                socket.SendTo(buffer, buffer.Length, 0, remoteEndPoint);
+                Console.WriteLine("Message received and sent back, to client!");
+            }
+            socket.Close();
+            Console.WriteLine("Server stoppped!");
+        }
+    }
+}
